cd913ee8ceb56efbd7d272acd56f31c72fb6925a
Velo <hartley.max@outlook.com>
2021-09-03 17:21:53 +0100

    create nav menu and test suite mount on dashboard

---
+++ src/components/NavMenu/index.test.js
@@ -0,0 +1,29 @@
+import NavMenu from '.';
+import { screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('Profile Page', () => {
+	beforeEach(() => {
+		render(<NavMenu />, { wrapper: MemoryRouter });
+	});
+
+	it('Renders a navigation menu', () => {
+		let nav = screen.getByRole('navigation');
+		expect(nav).toBeInTheDocument();
+	});
+
+	it('Renders a list of navigation links', () => {
+		let list = screen.getByRole('list');
+		expect(list).toBeInTheDocument();
+	});
+
+	it('Renders five page links', () => {
+		let links = screen.getAllByRole('link');
+		expect(links.length).toBe(5);
+	});
+
+	it('Renders a logout button', () => {
+		let logoutButton = screen.getByRole('button');
+		expect(logoutButton).toBeInTheDocument();
+	});
+});
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { NavMenu } from '../../components';
 
 export default () => {
 	return (
-		<main>
-			<h1>Hello Dashboard!</h1>;
-		</main>
+		<>
+			<NavMenu />
+			<main>
+				<h1>Hello Dashboard!</h1>;
+			</main>
+		</>
 	);
 };
