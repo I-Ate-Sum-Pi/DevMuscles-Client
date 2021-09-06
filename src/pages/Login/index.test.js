d9091dc44a450d39136a762b4d7096008bcce248
Velo <hartley.max@outlook.com>
2021-09-06 10:45:15 +0100

    stub login page and test suite

---
+++ src/pages/Login/index.test.js
@@ -0,0 +1,32 @@
+import Login from '.';
+import { MemoryRouter } from 'react-router-dom';
+import { screen } from '@testing-library/react';
+
+describe('Login Page', () => {
+	beforeEach(() => {
+		render(<Login />, { wrapper: MemoryRouter });
+	});
+
+	it('Renders a heading', () => {
+		let heading = screen.getByRole('heading');
+		expect(heading).toBeInTheDocument();
+		expect(heading.textContent).toBe('Login');
+	});
+
+	it('Renders a login form', () => {
+		let form = screen.getByRole('form');
+		expect(form).toBeInTheDocument();
+	});
+
+	it('Renders a go back link', () => {
+		let goBackLink = screen.getByRole('link', { name: 'return to landing page' });
+		expect(goBackLink).toBeInTheDocument();
+		expect(goBackLink.textContent).toBe('Go back');
+	});
+
+	it('Renders a link to registration page', () => {
+		let registLink = screen.getByRole('link', { name: 'go to register page' });
+		expect(registLink).toBeInTheDocument();
+		expect(registLink.textContent).toBe("Don't have an account yet? Register here");
+	});
+});
