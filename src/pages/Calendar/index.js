f287b69d9e7a620fd6363945a89e165f6a08a888
akashkhambay <akashkhambay@hotmail.co.uk>
2021-09-07 12:21:01 +0100

    Create dateState and setDateState hooks

--- src/pages/Calendar/index.js
+++ src/pages/Calendar/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import moment from 'moment';
 
 export default () => {
+	const [dateState, setDateState] = useState(new Date());
 	return (
 		<main>
 			<h1>Hello Calendar!</h1>;
