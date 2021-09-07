2802520507db139b35a62bbfce98b6628e6ce36e
Velo <hartley.max@outlook.com>
2021-09-07 22:12:08 +0100

    abstract dashboard greeting to component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Calendar } from './Calendar';
 export { default as DashboardConsole } from './DashboardConsole';
+export { default as DashboardGreeting } from './DashboardGreeting';
 export { default as NavMenu } from './NavMenu';
