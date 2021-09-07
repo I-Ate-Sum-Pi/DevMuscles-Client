8f387ca5caa7d7a24d6d44f3bb607b35677a0fd2
akashkhambay <akashkhambay@hotmail.co.uk>
2021-09-07 12:12:05 +0100

    Add Calendar component so now it should be rendered to the page

--- src/pages/Calendar/index.js
+++ src/pages/Calendar/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 export default () => {
 	return (
 		<main>
 			<h1>Hello Calendar!</h1>;
+			<Calendar />
 		</main>
 	);
 };
