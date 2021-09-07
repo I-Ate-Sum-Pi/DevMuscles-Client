4be871ee16db04b6ee0c6b41c60eaf6d51ac0aa7
akashkhambay <akashkhambay@hotmail.co.uk>
2021-09-07 12:55:52 +0100

    Add test for calendar component

---
+++ src/pages/Calendar/index.test.js
@@ -0,0 +1,15 @@
+import Calendar from '.';
+import { MemoryRouter } from 'react-router-dom';
+import { screen } from '@testing-library/react';
+
+describe('Calendar Page', () => {
+	beforeEach(() => {
+		render(<Calendar />, { wrapper: MemoryRouter });
+	});
+
+	it('Renders a heading', () => {
+		let heading = screen.getByRole('heading');
+		expect(heading).toBeInTheDocument();
+		expect(heading.textContent).toBe('Hello Calendar!');
+	});
+});
