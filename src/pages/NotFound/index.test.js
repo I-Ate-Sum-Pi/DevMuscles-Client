b71efdf5344b26fcf78f6abc1dcf86b894a77024
Velo <hartley.max@outlook.com>
2021-09-08 10:17:34 +0100

    create not found page and test suite

---
+++ src/pages/NotFound/index.test.js
@@ -0,0 +1,20 @@
+import NotFound from '.';
+import { MemoryRouter } from 'react-router-dom';
+import { screen } from '@testing-library/react';
+
+describe('NotFound Page', () => {
+	beforeEach(() => {
+		renderWithAuth(<NotFound />, { wrapper: MemoryRouter });
+	});
+
+	it('Renders a heading', () => {
+		let heading = screen.getByRole('heading');
+		expect(heading).toBeInTheDocument();
+	});
+
+	it('Renders a link to the landing page', () => {
+		let link = screen.getByRole('link');
+		expect(link).toBeInTheDocument();
+		expect(link.href).toBe('http://localhost/');
+	});
+});
