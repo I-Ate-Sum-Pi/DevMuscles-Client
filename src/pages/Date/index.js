deaa9f23348114de20c0ee7aff75012ba8279d56
akashkhambay <akashkhambay@hotmail.co.uk>
2021-09-08 15:17:58 +0100

    Refactor dates page, so that it is only rendering the layout component

--- src/pages/Date/index.js
+++ src/pages/Date/index.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-
-const API_ROOT = process.env.REACT_APP_API_ROOT
-	? process.env.REACT_APP_API_ROOT
-	: 'https://devmuscles.herokuapp.com';
-
-const { date } = useParams();
-
-try {
-	const { data: dateWorkout } = await axios.get(`${API_ROOT}/users/dates?${date}`);
-	console.log(dateWorkout);
-} catch (err) {
-	console.error(err);
-	return null;
-}
+import { DateWorkout } from '../../layout';
 
 export default () => {
+	const { date } = useParams();
 	return (
 		<main>
-			<h1> `${date}` </h1>;
-			<h2>This is the workouts for a paticular user on a paticular date `${dateWorkout}`</h2>
+			<h1>{date}</h1>
+			<DateWorkout />
 		</main>
 	);
 };
