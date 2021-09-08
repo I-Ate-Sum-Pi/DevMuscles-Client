563fc937c96d23c62cfad788627bfe91e5c20628
JawwadUddin <m.m.j.uddin12@gmail.com>
2021-09-08 18:56:27 +0100

    added navmenu and Workout components to workouts page

--- src/pages/Workouts/index.js
+++ src/pages/Workouts/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import { Workout , NavMenu } from '../../components'
+import styles from './styles.module.css';
 export default () => {
 	return (
-		<main>
-			<h1>Hello Workouts!</h1>;
-		</main>
+		<>
+			<NavMenu />
+			<main className={styles.main}>
+				<h1>Hello Workouts!</h1>
+				<Workout />
+			</main>
+		</>
 	);
 };
