3db5c9c8ae59a0c3fe417873e51afb5312397e60
Velo <hartley.max@outlook.com>
2021-09-08 11:14:51 +0100

    add more comprehensive dashboard greeting tests

--- src/components/DashboardGreeting/index.test.js
+++ src/components/DashboardGreeting/index.test.js
@@ -3,12 +3,22 @@ import { screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
 describe('DashboardGreeting component', () => {
-	beforeEach(() => {
-		renderWithAuthProvider(<DashboardGreeting />, { wrapper: MemoryRouter });
-	});
-
-	it('Renders a greeting', () => {
+	it('Greets the user', () => {
+		renderWithAuthContext(
+			<DashboardGreeting />,
+			{ currentUser: { username: 'test user' } },
+			{ wrapper: MemoryRouter }
+		);
 		const heading = screen.getByRole('heading');
 		expect(heading).toBeInTheDocument();
+		expect(heading).toHaveTextContent('Hi, test user!');
+	});
+
+	it('Renders and empty greeting if there is no user', () => {
+		renderWithAuthContext(<DashboardGreeting />, { currentUser: null }, { wrapper: MemoryRouter });
+
+		const heading = screen.getByRole('heading');
+		expect(heading).toBeInTheDocument();
+		expect(heading).toHaveTextContent('Hi, !');
 	});
 });
