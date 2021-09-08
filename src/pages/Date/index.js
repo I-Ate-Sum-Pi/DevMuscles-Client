dd6c5f5dbaf7564c120d15465de32c5c76f608f8
akashkhambay <akashkhambay@hotmail.co.uk>
2021-09-08 13:01:19 +0100

    Got a fetch request to the server to get the date workouts data

--- src/pages/Date/index.js
+++ src/pages/Date/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+const API_ROOT = process.env.REACT_APP_API_ROOT
+	? process.env.REACT_APP_API_ROOT
+	: 'https://devmuscles.herokuapp.com';
+
+const { date } = useParams();
+
+try {
+	const { data: dateWorkout } = await axios.get(`${API_ROOT}/users/dates?${date}`);
+	console.log(dateWorkout);
+} catch (err) {
+	console.error(err);
+	return null;
+}
+
 export default () => {
-	const API_ROOT = process.env.REACT_APP_API_ROOT
-		? process.env.REACT_APP_API_ROOT
-		: 'https://devmuscles.herokuapp.com';
-
-	const { date } = useParams();
-
 	return (
 		<main>
-			<h1>Want to the put the date that the user tapped on here!</h1>;
-			<h2>Put a Modal here for the workouts that the user has scheduled</h2>
+			<h1> `${date}` </h1>;
+			<h2>This is the workouts for a paticular user on a paticular date `${dateWorkout}`</h2>
 		</main>
 	);
 };
