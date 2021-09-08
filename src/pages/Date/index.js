9b97d0ddf014e6eb87c050e410570925094c14f0
akashkhambay <akashkhambay@hotmail.co.uk>
2021-09-08 12:45:04 +0100

    add API_ROOT variable

--- src/pages/Date/index.js
+++ src/pages/Date/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 export default () => {
+	const API_ROOT = process.env.REACT_APP_API_ROOT
+		? process.env.REACT_APP_API_ROOT
+		: 'https://devmuscles.herokuapp.com';
+
+	const { date } = useParams();
+
 	return (
 		<main>
 			<h1>Want to the put the date that the user tapped on here!</h1>;
