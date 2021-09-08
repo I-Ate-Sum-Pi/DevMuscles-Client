ed7cfaf8807c7b9def315757b22f25a09d5f4c90
Velo <hartley.max@outlook.com>
2021-09-08 23:36:02 +0100

    fix calendar test suites

--- src/components/Calendar/index.test.js
+++ src/components/Calendar/index.test.js
@@ -1,37 +1,89 @@
 import Calendar from '.';
-import { render, screen } from '@testing-library/react';
+import { act, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import axios from 'axios';
+
+jest.mock('axios');
 
 describe('Calendar', () => {
+	const mockValue = {
+		currentUser: {
+			id: 'test',
+		},
+	};
+
 	beforeEach(() => {
-		render(<Calendar />, { wrapper: MemoryRouter });
+		jest.resetAllMocks();
 	});
 
 	it('Renders a toggle previous year button', () => {
+		axios.get.mockResolvedValueOnce({ data: [] });
+		act(() => {
+			renderWithAuthContext(<Calendar />, mockValue, { wrapper: MemoryRouter });
+		});
+		waitFor(() => {
+			expect(axios.get).toHaveBeenCalled();
+		});
 		const togglePrevYearButton = screen.getByText('«');
 		expect(togglePrevYearButton).toBeInTheDocument();
 	});
 
 	it('Renders a toggle previous month button', () => {
+		axios.get.mockResolvedValueOnce({ data: [] });
+		act(() => {
+			renderWithAuthContext(<Calendar />, mockValue, { wrapper: MemoryRouter });
+		});
+		waitFor(() => {
+			expect(axios.get).toHaveBeenCalled();
+		});
 		const togglePrevMonthButton = screen.getByText('‹');
 		expect(togglePrevMonthButton).toBeInTheDocument();
 	});
 
 	it('Renders a toggle next year button', () => {
+		axios.get.mockResolvedValueOnce({ data: [] });
+		act(() => {
+			renderWithAuthContext(<Calendar />, mockValue, { wrapper: MemoryRouter });
+		});
+		waitFor(() => {
+			expect(axios.get).toHaveBeenCalled();
+		});
 		const toggleNextYearButton = screen.getByText('»');
 		expect(toggleNextYearButton).toBeInTheDocument();
 	});
 
 	it('Renders a toggle next month button', () => {
+		axios.get.mockResolvedValueOnce({ data: [] });
+		act(() => {
+			renderWithAuthContext(<Calendar />, mockValue, { wrapper: MemoryRouter });
+		});
+		waitFor(() => {
+			expect(axios.get).toHaveBeenCalled();
+		});
 		const toggleNextMonthButton = screen.getByText('›');
 		expect(toggleNextMonthButton).toBeInTheDocument();
 	});
+
 	it('Renders the days abbreivation on the calendar', () => {
+		axios.get.mockResolvedValueOnce({ data: [] });
+		act(() => {
+			renderWithAuthContext(<Calendar />, mockValue, { wrapper: MemoryRouter });
+		});
+		waitFor(() => {
+			expect(axios.get).toHaveBeenCalled();
+		});
 		const DayAbbreviation = screen.getByText('Mon');
 		expect(DayAbbreviation).toBeInTheDocument();
 	});
 
 	it('Renders the month at the top of the calendar', () => {
+		axios.get.mockResolvedValueOnce({ data: [] });
+		act(() => {
+			renderWithAuthContext(<Calendar />, mockValue, { wrapper: MemoryRouter });
+		});
+		waitFor(() => {
+			expect(axios.get).toHaveBeenCalled();
+		});
 		const MonthAtTopOfPage = screen.getByText('September 2021');
 		expect(MonthAtTopOfPage).toBeInTheDocument();
 	});
--- src/pages/Calendar/index.test.js
+++ src/pages/Calendar/index.test.js
@@ -2,15 +2,19 @@ import Calendar from '.';
 import { MemoryRouter } from 'react-router-dom';
 import { screen } from '@testing-library/react';
 
+jest.mock('../../components/Calendar/index.js', () => () => (
+	<section aria-label="Calendar"></section>
+));
+
 describe('Calendar Page', () => {
 	beforeEach(() => {
-		render(<Calendar />, { wrapper: MemoryRouter });
+		renderWithAuthProvider(<Calendar />, { wrapper: MemoryRouter });
 	});
 
 	it('Renders a heading', () => {
 		const heading = screen.getByRole('heading');
 		expect(heading).toBeInTheDocument();
-		expect(heading.textContent).toBe('Hello Calendar!');
+		expect(heading.textContent).toBe('Your Workout Calendar');
 	});
 
 	it('Renders a calendar', () => {
