dd943cc53f6a299e69b5168c884caba5e3dc9d00
Velo <hartley.max@outlook.com>
2021-09-09 21:27:13 +0100

    style profile page

--- src/components/ProfileInfo/index.js
+++ src/components/ProfileInfo/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './styles.module.css';
 import { useAuth } from '../../contexts/AuthContext';
 
 export default () => {
 	const { currentUser } = useAuth();
 	return (
-		<section aria-label="user information">
+		<section aria-label="user information" className={styles.section}>
 			<p aria-label="username">Username: {currentUser.username}</p>
 			<p aria-label="email">Email: {currentUser.email}</p>
 		</section>
