83feae33a9fdafdb3146a3ced0b520b69277c304
Velo <hartley.max@outlook.com>
2021-09-09 22:30:31 +0100

    refine and style calendar page

--- src/pages/Calendar/index.js
+++ src/pages/Calendar/index.js
@@ -9,6 +9,13 @@ export default () => {
 			<main className={styles.main}>
 				<h1>Your Workout Calendar</h1>
 				<Calendar />
+				<div role="presentation" className={styles.calendarInfo}>
+					<p>Select a day to schedule a workout.</p>
+					<p>
+						Days with a <span className={styles.greenBorder}>green border</span> have a workout
+						scheduled!
+					</p>
+				</div>
 			</main>
 		</>
 	);
